d1adadc75203c5a0d62e8351fc21605a95fd6cbc
Felippe Ladeira <felippe@filteragency.com>
2026-01-23 15:03:17 +0000

    - Added support for https on customer app

--- jdw/builds/customer-app/entrypoint.sh
+++ jdw/builds/customer-app/entrypoint.sh
@@ -5,4 +5,4 @@ set -e
 export ENT_NATIVE_KEY="$(cat /run/secrets/ionic_ent_key)"
 
 cd /app
-npm install && NODE_OPTIONS='--inspect' node_modules/.bin/ionic serve --host=${HOSTNAME} --port=${PORT}
+npm install && NODE_OPTIONS='--inspect' node_modules/.bin/ionic serve --ssl --host=${HOSTNAME} --port=${APP_PORT}
--- jdw/builds/manager-app/entrypoint.sh
+++ jdw/builds/manager-app/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 cd /app
 npm install && NODE_OPTIONS='--inspect' node_modules/.bin/next dev -H ${HOSTNAME}
-#npm install && node_modules/.bin/next build && node_modules/.bin/next start -H app.jdw.local
+#npm install && node_modules/.bin/next build && node_modules/.bin/next start -H ${HOSTNAME}
